baf1c432ba9488d62e16e9bdc258e1ce90e02084
ANJALI KUMARI_MCA_2024_26_07_UEMK <anjalikumar151322@gmail.com>
2025-03-04 18:57:47 +0530

    Create 5.py

---
+++ PPT3/5.py
@@ -0,0 +1,5 @@
+5. Write a program that prints a list where the values are square of numbers between 5000 and
+7000 (both included).
+for num in range(5000, 7001):
+if (num**0.5).is_integer():
+print(num)
