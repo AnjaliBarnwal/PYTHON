3c3dda3f2d0b6b4a85f132850bdabcd7723ad7fb
ANJALI KUMARI_MCA_2024_26_07_UEMK <anjalikumar151322@gmail.com>
2025-03-04 21:07:22 +0530

    Create 17.py

---
+++ PPT3/17.py
@@ -0,0 +1,8 @@
+17. Use the string formatting method to display the following:
+radius = 10
+area = 3.14 * radius ** 2
+The area of a circle with radius 10 is 314 meters square.
+radius = 10
+area = 3.14 * radius ** 2
+
+print("The area of a circle with radius {} is {} meters square.".format(radius, area))
