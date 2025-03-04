7af0d2bd3a3371f5daac723e5f192a437608771b
ANJALI KUMARI_MCA_2024_26_07_UEMK <anjalikumar151322@gmail.com>
2025-03-04 18:56:22 +0530

    Create 3.py

---
+++ PPT3/3.py
@@ -0,0 +1,4 @@
+3. Write a program to input a 10 digit number and print alternate digits.
+number = input("Enter a 10-digit number: ")
+alternate_digits = number[::2]
+print("Alternate digits:", alternate_digits)
