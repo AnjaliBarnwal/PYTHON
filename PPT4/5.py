d7b5aafe3008b0cd34e40b2923142e47690a3ab1
ANJALI KUMARI_MCA_2024_26_07_UEMK <anjalikumar151322@gmail.com>
2025-03-04 21:15:43 +0530

    Update 5.py

--- PPT4/5.py
+++ PPT4/5.py
@@ -3,3 +3,9 @@ input_string = input("Enter a string:")
 is_palindrome = True
 for i in range(len(input_string) // 2):
 if input_string[i] != input_string[len(input_string) - 1 - i]:
+  is_palindrome = False
+break
+if is_palindrome:
+print("The string is a palindrome or symmetrical.")
+else:
+print("The string is not a palindrome or symmetrical.")
