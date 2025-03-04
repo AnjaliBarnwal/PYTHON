d982ac92edd10db8a3445a11f16c15a34a367c3b
ANJALI KUMARI_MCA_2024_26_07_UEMK <anjalikumar151322@gmail.com>
2025-03-04 21:18:33 +0530

    Create 9.py

---
+++ PPT4/9.py
@@ -0,0 +1,7 @@
+#wap in python to cloning and copy a list
+original_list = input("Enter the elements of the list: ").split()
+copied_list = []
+for item in original_list:
+copied_list.append(item)
+print("Original List:", original_list)
+print("Copied List:", copied_list)
